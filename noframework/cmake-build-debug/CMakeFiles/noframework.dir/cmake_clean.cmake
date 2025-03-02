afd4217548a9ce9247dccde1c56301932b8e6dd1
Kaci - Lukyho laptop <lukaskorepa@gmail.com>
2025-03-02 02:44:27 +0100

    Add noframework and set up

---
+++ noframework/cmake-build-debug/CMakeFiles/noframework.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/noframework.dir/main.cpp.o"
+  "CMakeFiles/noframework.dir/main.cpp.o.d"
+  "noframework"
+  "noframework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/noframework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ noframework/cmake-build-debug/CMakeFiles/noframework.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for noframework.
+# This may be replaced when dependencies are built.
